4dca3afc512c90179a0e451ae2f4383e62bffec2
ssoliva <sjolivares@uc.cl>
2023-11-04 19:48:25 -0300

    App base build

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,17 @@
-// import express from "express";
-// import dotenv from "dotenv";
+import express from 'express';
+import dotenv from 'dotenv';
+
+const app = express();
+
+dotenv.config();
+
+app.set("port", process.env.PORT);
 
 
-console.log(123)
+app.get('/', (req, res) => {
+  res.send('Hello NOD Readers!');
+});
+
+app.listen(app.get('port'), () => {
+return console.log(`Express server is listening at http://localhost:${app.get('port')} 🚀`);
+});
